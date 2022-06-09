5b1e5b4625357d2eeda7c66a80b9f4f627b959a1
EvyaterGuyAvrahamCom <102894435+EvyaterGuyAvrahamCom@users.noreply.github.com>
2022-06-09 10:28:12 +0300

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
+
+const Header = () => {
+  return (
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+          alt="fb"
+        />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="hidden md:inline-flex flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            placeholder="Search Facebook"
+            type="text"
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} active />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* <Image
+          src="https://www.facebook.com/photo.php?fbid=10158993533511947&set=pb.563001946.-2207520000..&type=3"
+          alt="profile-pic"
+          w={40}
+          h={40}
+        /> */}
+        <p className="whitespace-nowrap font-semibold pr-3">Evyatar Haim</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+  nextConfig,
+}
+
+
